d3d5a4e3f9cfbee976d5af57084466f732ef8871
Zach Donovan <zach@zachdonovan.net>
2015-04-11 23:56:37 -0400

    first stab at TDD

---
+++ test/test.js
@@ -0,0 +1,25 @@
+/*
+ * TRIPLE OPTION
+ *
+ * an homage to Ten-Yard Fight.
+ * by Zachary Donovan
+ *
+ * Triple Option is a radically restricted
+ * American football simulation. In the game,
+ * the player carries out a series of
+ * simple plays seeking to evade the defense, 
+ * gain as many yards as possible, score a  
+ * touchdown, etc.
+ *
+ * Play consists of groups of four downs in which
+ * player completes a series of plays with brief
+ * pauses before the start of the next play.
+ * The player is limited to the three plays for
+ * which the game is named: handoff to a running
+ * back, pass to a wide receiver, or run the
+ * ball as the quarterback. There is one special
+ * play available as well, which doubles as a
+ * punt/field goal attempt, which the player
+ * automatically undertakes on fourth down.
+ *
+ */
